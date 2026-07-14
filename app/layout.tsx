4423e3c4aca8b5844cb1a0cdc50128e0ba0ea64c
Deepak Verma <deepakrvermaa@gmail.com>
2026-07-14 10:35:03 +0530

    fixes bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Inter, JetBrains_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/next';
 import './globals.css'
 
 const inter = Inter({
@@ -24,10 +25,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
+
     <ClerkProvider>
       <html lang="en" className="dark overflow-hidden">
         <body className={`${inter.variable} ${jetbrainsMono.variable} antialiased bg-bg text-text-primary`}>
           {children}
+          <Analytics />
         </body>
       </html>
     </ClerkProvider>
